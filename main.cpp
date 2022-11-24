5479e14a5840c16b7f37e7ccaef8757adee6959b
MatteoBottoo <115472031+MatteoBottoo@users.noreply.github.com>
2022-11-24 22:54:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
+   int a=0,b=1,i=2,n=0;
+   
+    cin>>n;
+   
+    if (n>=2){
+       
+        cout<<b<<endl;
+       
+        while(i++<=n){
+           
+            cout<<a+b<<endl;
+           
+            if(a<b){
+               
+                a+=b;
+               
+            }else{b+=a;}
+        }
+       
+    }else{
+       
+        cout<<"errore"<<endl;
+       
+    }   
    return 0;
 }
